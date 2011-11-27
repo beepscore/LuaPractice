6ae4df446565ff4bbe62fff4b48b4381667f6d2e
beepscore <support@beepscore.com>
2011-11-26 20:31:28 -0800

    Correct and add comments.

--- factorial.lua
+++ factorial.lua
@@ -1,19 +1,23 @@
 #!/usr/local/bin/lua
 
--- first line of file is path to lua interpreter, allows 
--- run script by typing ./<filename> instead of typing lua <filename>
---
+-- lua script copyright 2011 Beepscore LLC
+-- First line of file is path to lua interpreter
+-- allows run script by typing <filename> instead of typing lua <filename>
+-- 
 -- Initial one time setup:
--- set $PATH to include directory that contains this script
--- open terminal shell, give user permission to execute file
--- chmod u+x factorial.lua
+--   Script must be in a directory visible in $PATH, e.g. /Users/stevebaker/bin
+--   Can edit .bash_profile to add a directory to the path.
+--   User must have permission to execute file.
+--   In terminal shell:
+--     chmod u+x  factorial.lua
+-- /////////////////////////////
 --
--- To use:
--- ./factorial.lua
+-- To run script from any directory:
+--  factorial.lua
 --
 -- References:
 -- http://www.lua.org/pil/1.4.html
--- # http://stackoverflow.com/questions/166347/how-do-i-use-ruby-for-shell-scripting
+-- http://stackoverflow.com/questions/166347/how-do-i-use-ruby-for-shell-scripting
 -- http://en.wikibooks.org/wiki/Ruby_Programming/Hello_world
 -- http://linux-certification.blogspot.com/2008/10/using-shell-commands-in-ruby.html
 
