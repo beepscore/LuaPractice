78f8b430accfdd252b8d6174328f14315c278d0b
beepscore <support@beepscore.com>
2011-11-27 19:55:57 -0800

    Add functions, format output with additional print statements.

--- typesTutorial.lua
+++ typesTutorial.lua
@@ -7,11 +7,13 @@
 print (true ~= false)
 
 -- table
+print("tables")
 x={}
 print(x)
 -- table: 0x7f947b40db00
 x = {value=123, text="hello"}
 print(x.value)
+print()
 -- 123
 print(x.text)
 -- hello
@@ -19,3 +21,11 @@ y =  { const = { name="Pi", value=3.1415927 },
        const2 = { name="light speed", value=3e8 } }
 print(y.const.name)
 print(y.const2.value)
+print()
+-- functions
+print("functions")
+function foo()
+    print("hello")
+end
+foo()
+print(foo)
