c050f9c6d178167c824a6934dad536aa20f89715
beepscore <support@beepscore.com>
2012-03-19 21:48:57 -0700

    Add shebang line. Revise print statements. Add comments.

--- typesTutorial.lua
+++ typesTutorial.lua
@@ -1,3 +1,5 @@
+#!/usr/bin/env lua
+
 -- lua script copyright 2011 Beepscore LLC
 
 -- References:
@@ -12,16 +14,17 @@ print()
 print("tables")
 x={}
 print(x)
--- table: 0x7f947b40db00
+-- table: 0x7fe4d3c06c30
+
 x = {value=123, text="hello"}
-print(x.value)
--- 123
-print(x.text)
--- hello
+print(x)
+-- table: 0x7fe4d3c06da0
+print("x.value: " .. x.value, "x.text: " .. x.text)
+
 y =  { const = { name="Pi", value=3.1415927 },
        const2 = { name="light speed", value=3e8 } }
-print(y.const.name)
-print(y.const2.value)
+print(y.const.name, y.const.value)
+print(y.const2.name, y.const2.value)
 print()
 
 -- functions
@@ -50,6 +53,10 @@ print()
 print("querying type")
 x = "123"
 print(x, type(x))
+-- 123	string
+
+-- In Lua, string + number -> number!
+-- In JavaScript, string + number -> string.
 x = x + 7
 print(x, type(x))
-
+-- 130	number
